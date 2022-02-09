c9956b4897186165103de1c8513e6ed6aa449113
DSchirmer1987 <dschirmer1987@gmail.com>
2022-02-09 11:11:31 +0100

    JSON Reader Beispiel

---
+++ Network/src/useJSON/JSONReader.java
@@ -0,0 +1,60 @@
+package useJSON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JSONReader {
+	private URL url;
+	
+	public JSONReader(String url) throws MalformedURLException {
+		this.url = new URL(url);
+	}
+	
+	public JSONObject readJSONFromUrl() throws IOException {
+		InputStream input = this.url.openStream();
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName("UTF-8")));
+			StringBuilder str = new StringBuilder();
+			int charTemp;
+			do {
+				charTemp = reader.read();
+				str.append((char) charTemp);
+			} while (charTemp != -1);
+			JSONObject json = new JSONObject(str.toString());
+			return json;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			input.close();
+		}
+	}
+	
+	public JSONArray readJSONArrayFromURL() throws IOException {
+		InputStream input = this.url.openStream();
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName("UTF-8")));
+			StringBuilder str = new StringBuilder();
+			int charTemp;
+			do {
+				charTemp = reader.read();
+				str.append((char) charTemp);
+			} while (charTemp != -1);
+			JSONArray json = new JSONArray(str.toString());
+			return json;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			input.close();
+		}
+	}
+}
---
+++ Network/src/useJSON/ReadJSONUrl.java
@@ -0,0 +1,27 @@
+package useJSON;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ReadJSONUrl {
+
+	public static void main(String[] args) {
+		String urlArray = "https://randomname.de/?format=json&count=10";
+		String url = "http://ip.jsontest.com/";
+		try {
+			JSONReader reader = new JSONReader(url);
+			JSONObject myJSON = reader.readJSONFromUrl();
+			System.out.println(myJSON.toString());
+			JSONReader readerArray = new JSONReader(urlArray);
+			JSONArray myJSONArray = readerArray.readJSONArrayFromURL();
+			System.out.println(myJSONArray.toString());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
