707dd5ecdc8a978423eb9e51dbdabda6935c1fd1
DSchirmer1987 <dschirmer1987@gmail.com>
2022-02-10 08:50:37 +0100

    Socket Beispiel

---
+++ Network/src/sockets/SocketTest.java
@@ -0,0 +1,25 @@
+package sockets;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketTest {
+	public static void main(String[] args) {
+		try(Socket socket = new Socket("naughty.java.club", 80)){
+//			Socket socket2 = new Socket(socket.getInetAddress(), socket.getPort());
+			
+			System.out.println(InetAddress.getLocalHost());
+			System.out.println(socket.getLocalAddress());
+			System.out.println(socket.getLocalPort());
+			System.out.println(socket.getRemoteSocketAddress());
+			System.out.println(socket.getPort());
+			System.out.println("Verbindung hergestellt");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
