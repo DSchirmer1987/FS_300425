6e3981f8069f497038190e3451aac69e97d3dc2c
DSchirmer1987 <dschirmer1987@gmail.com>
2022-02-10 10:41:20 +0100

    MulClient Beispiel

--- Network/src/sockets/SocketTest.java
+++ Network/src/sockets/SocketTest.java
@@ -7,6 +7,7 @@ import java.net.UnknownHostException;
 
 public class SocketTest {
 	public static void main(String[] args) {
+		System.err.println("Dies ist ein fehler!");
 		try(Socket socket = new Socket("192.168.226.132", 12345)){
 //			Socket socket2 = new Socket(socket.getInetAddress(), socket.getPort());
 			
---
+++ Network/src/sockets/client/MulClient.java
@@ -0,0 +1,33 @@
+package sockets.client;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class MulClient {
+	private int port;
+	public MulClient(int port, String factor1, String factor2) {
+		this.port = port;
+		this.multiply(factor1, factor2);
+	}
+	
+	private void multiply(String factor1, String factor2) {
+		try(Socket socket = new Socket("192.168.226.132", this.port);
+			Scanner in = new Scanner(socket.getInputStream(), "UTF-8");
+			Writer osw = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+			PrintWriter out = new PrintWriter(osw, true)){
+			
+			out.println(factor1);
+			out.println(factor2);
+			System.out.println("Ergebnis: " + in.nextLine());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
