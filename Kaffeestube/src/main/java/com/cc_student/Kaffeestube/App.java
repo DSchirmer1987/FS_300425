05f6d389332f76ebde8d75a2e51a0f6f23a1694f
DSchirmer1987 <dschirmer1987@gmail.com>
2022-02-15 09:27:13 +0100

    Maven-Beispiel

---
+++ Kaffeestube/src/main/java/com/cc_student/Kaffeestube/App.java
@@ -0,0 +1,13 @@
+package com.cc_student.Kaffeestube;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
