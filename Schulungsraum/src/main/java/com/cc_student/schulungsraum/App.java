4a8949b7dbd45a53a4087cc4d908dcc9be85b19c
DSchirmer1987 <dschirmer1987@gmail.com>
2022-02-16 11:23:32 +0100

    Maven Properties

---
+++ Schulungsraum/src/main/java/com/cc_student/schulungsraum/App.java
@@ -0,0 +1,9 @@
+package com.cc_student.schulungsraum;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt");
+	}
+
+}
---
+++ Schulungsraum/src/test/java/com/cc_student/schulungsraum/AppTest.java
@@ -0,0 +1,12 @@
+package com.cc_student.schulungsraum;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void shouldBeOk() {
+		assertTrue(true);
+	}
+}
